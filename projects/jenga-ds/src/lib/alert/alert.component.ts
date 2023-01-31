3bfb89565b10daf2ebffb6ccbc745bdbaaf64e91
kihuha <29231722+kihuha@users.noreply.github.com>
2023-01-31 16:55:30 +0300

    Made progress on alert component

--- projects/jenga-ds/src/lib/alert/alert.component.ts
+++ projects/jenga-ds/src/lib/alert/alert.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'jenga-alert',
   templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss'],
 })
-export class Alert {}
+export class Alert {
+  @Input()
+  type: 'filled' | 'tonal' = 'filled';
+}
